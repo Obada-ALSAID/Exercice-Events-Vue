560c5395475ef09d37375d6dd0978183397a28dd
Obada ALSAID MHIA <obadah.sayed@gmail.com>
2025-12-31 05:34:59 +0100

    Adding events to HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,40 @@
 <script setup>
+  import { ref } from 'vue';
   import EventCard from '@/components/EventCard.vue';
+
+  const events = ref([
+    {
+      id: 1,
+      time: '18:00',
+      date: '2025-12-10',
+      title: 'Soirée jeux de société',
+    },
+    {
+      id: 2,
+      time: '14:30',
+      date: '2025-12-15',
+      title: 'Atelier Vue 3',
+    },
+    {
+      id: 3,
+      time: '09:00',
+      date: '2025-12-20',
+      title: 'Petit-déjeuner de classe',
+    },
+  ])
 </script>
 
 <template>
-  <EventCard />
+  <div class="events">
+    <EventCard />
+  </div>
 </template>
+
+
+<style scoped>
+  .events {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+</style>
